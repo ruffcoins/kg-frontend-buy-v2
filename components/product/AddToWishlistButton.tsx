79791cbaab765befbeae900dc847d795201662f8
cprincec <chukwuprincec@gmail.com>
2024-11-04 23:14:22 +0100

    Hid the addToWishlistButton when user is not logged in

--- components/product/AddToWishlistButton.tsx
+++ components/product/AddToWishlistButton.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Dispatch, SetStateAction, useState } from "react";
+import { Dispatch, SetStateAction } from "react";
 import Image from "next/image";
 import EmptyHeart from "@/public/images/empty-heart.svg";
 import FilledHeart from "@/public/images/filled-heart.svg";
@@ -8,87 +8,60 @@ import { cn } from "@/lib/utils";
 import useAddToWishlist from "@/hooks/mutation/wishlist/addToWishlist";
 import useRemoveFromWishlist from "@/hooks/mutation/wishlist/removeFromWishlist";
 import useAuth from "@/hooks/useAuth";
-import { AuthDialog } from "../auth/dialogs/AuthDialog";
-import EnterOtp from "../auth/dialogs/EnterOtp";
 
 const AddToWishlistButton = ({
-  classNames,
-  isOnMyWishList,
-  id,
-  name,
-  price,
-  imageUrl,
+    classNames,
+    isOnMyWishList,
+    id,
+    name,
+    price,
+    imageUrl,
 }: {
-  classNames?: string;
-  isOnMyWishList: boolean;
-  setIsOnMyWishList: Dispatch<SetStateAction<boolean>>;
-  id: string;
-  name: string;
-  price: number;
-  imageUrl: string;
+    classNames?: string;
+    isOnMyWishList: boolean;
+    setIsOnMyWishList: Dispatch<SetStateAction<boolean>>;
+    id: string;
+    name: string;
+    price: number;
+    imageUrl: string;
 }) => {
-  const [openAuthModal, setOpenAuthModal] = useState(false);
-  const [showOtpModal, setShowOtpModal] = useState(false);
-  const [email, setEmail] = useState("");
-  const [phone, setPhone] = useState("");
-  const { isLoggedIn } = useAuth();
-  const { addProductToWishlist } = useAddToWishlist();
-  const { removeItemFromWishlist } = useRemoveFromWishlist();
+    const { isLoggedIn } = useAuth();
+    const { addProductToWishlist } = useAddToWishlist();
+    const { removeItemFromWishlist } = useRemoveFromWishlist();
 
-  const handleAddToWishlist = () => {
+    const handleAddToWishlist = () => {
+        if (isOnMyWishList) {
+            removeItemFromWishlist(id);
+        } else {
+            addProductToWishlist({
+                productId: id,
+                name,
+                price: price,
+                productImage: imageUrl,
+            });
+        }
+    };
     if (!isLoggedIn) {
-      setOpenAuthModal(true);
-    } else {
-      if (isOnMyWishList) {
-        removeItemFromWishlist(id);
-      } else {
-        addProductToWishlist({
-          productId: id,
-          name,
-          price: price,
-          productImage: imageUrl,
-        });
-      }
+        return null;
     }
-  };
 
-  return (
-    <>
-      <div
-        className={cn(
-          "w-10 h-10 flex justify-center items-center rounded-full bg-white/80 border-[0.5px] border-white cursor-pointer",
-          classNames,
-        )}
-        onClick={handleAddToWishlist}
-      >
-        <Image
-          src={isOnMyWishList ? FilledHeart : EmptyHeart}
-          alt="empty heart image"
-          className="relative w-6 h-6 left-50"
-          width={24}
-          height={24}
-        />
-      </div>
-
-      {openAuthModal && (
-        <AuthDialog
-          openAuthModal={openAuthModal}
-          setOpenAuthModal={setOpenAuthModal}
-          setShowOtpModal={setShowOtpModal}
-          setEmail={setEmail}
-          setPhone={setPhone}
-        />
-      )}
-      {showOtpModal && (
-        <EnterOtp
-          open={showOtpModal}
-          setOpen={setShowOtpModal}
-          email={email}
-          phone={phone}
-        />
-      )}
-    </>
-  );
+    return (
+        <div
+            className={cn(
+                "w-10 h-10 flex justify-center items-center rounded-full bg-white/80 border-[0.5px] border-white cursor-pointer",
+                classNames
+            )}
+            onClick={handleAddToWishlist}
+        >
+            <Image
+                src={isOnMyWishList ? FilledHeart : EmptyHeart}
+                alt="empty heart image"
+                className="relative w-6 h-6 left-50"
+                width={24}
+                height={24}
+            />
+        </div>
+    );
 };
 
 export default AddToWishlistButton;
