8ba4c5bec340320c664549046e5d4ef761a6117a
cprincec <chukwuprincec@gmail.com>
2024-11-06 18:41:16 +0100

    updated category banners

--- app/category/[categoryName]/page.tsx
+++ app/category/[categoryName]/page.tsx
@@ -2,15 +2,25 @@ import InnerPageLayout from "@/components/layouts/InnerPageLayout";
 import Loader from "@/components/shared/Loader";
 import dynamic from "next/dynamic";
 import Image from "next/image";
-import DesktopCategoryBanner from "@/public/images/women-banner.svg";
+// import desktopCategoryBanner from "@/public/images/women-banner.svg";
 import MobileCategoryBanner from "@/public/images/category-banner.png";
+
+// Banner Images
+import MenBanner from "@/public/images/categories/categories_banners/Men.jpg";
+import WomenBanner from "@/public/images/categories/categories_banners/Women.jpg";
+import HomeAndGardenBanner from "@/public/images/categories/categories_banners/Home&Graden.jpg";
+import PhonesBanner from "@/public/images/categories/categories_banners/Phones&Tablets.jpg";
+import ElectronicsBanner from "@/public/images/categories/categories_banners/Electronics.jpg";
+import OfficeBanner from "@/public/images/categories/categories_banners/Office&Schools.jpg";
+import GroceriesBanner from "@/public/images/categories/categories_banners/Groceries&Drinks.jpg";
+import ChildrenBanner from "@/public/images/categories/categories_banners/Children&Toy.jpg";
 
 import { IProductDetailResponse } from "@/interfaces/responses/product.interface";
 import { postRequest } from "@/utils/apiCaller";
 import { ResolvingMetadata, Metadata } from "next";
 
 type Props = {
-  params: { categoryName: string };
+    params: { categoryName: string };
 };
 
 // export async function generateMetadata(
@@ -114,64 +124,80 @@ type Props = {
 // };
 // }
 
-const SelectedForYou = dynamic(
-  () => import("@/components/category/SelectedForYou"),
-  {
+const SelectedForYou = dynamic(() => import("@/components/category/SelectedForYou"), {
     ssr: false,
     loading: () => (
-      <div className="flex justify-center items-center h-[200px] w-screen">
-        <Loader />
-      </div>
+        <div className="flex justify-center items-center h-[200px] w-screen">
+            <Loader />
+        </div>
     ),
-  },
-);
+});
 
-const SubCategoriesList = dynamic(
-  () => import("@/components/category/SubCategoriesList"),
-  {
+const SubCategoriesList = dynamic(() => import("@/components/category/SubCategoriesList"), {
     ssr: false,
     loading: () => (
-      <div className="flex justify-center items-center h-[200px] w-screen">
-        <Loader />
-      </div>
+        <div className="flex justify-center items-center h-[200px] w-screen">
+            <Loader />
+        </div>
     ),
-  },
-);
+});
+
+// const Banner = dynamic(() =>  import("@/components/category/SelectedForYou"), {
+//     ssr: false,
+//     loading: () => (
+//         <div className="flex justify-center items-center h-[200px] w-screen">
+//             <Loader />
+//         </div>
+//     ),
+// });
 
 const Category = ({ params }: { params: { categoryName: string } }) => {
-  const { categoryName } = params;
+    const { categoryName } = params;
 
-  return (
-    <InnerPageLayout>
-      <div className="space-y-4 lg:mb-80 mb-20 mt-20">
-        <div className="lg:mx-8 rounded-lg space-y-6 lg:space-y-10 flex flex-col">
-          <div className="border h-[200px] lg:h-[280px] relative">
-            <Image
-              src={DesktopCategoryBanner}
-              alt="desktop category banner"
-              className="w-full h-full object-cover object-center hidden lg:block rounded-lg"
-              fill
-              sizes="100vw"
-              priority
-            />
-            <Image
-              src={MobileCategoryBanner}
-              alt="mobile category banner"
-              className="w-full h-full object-cover object-center lg:hidden"
-              fill
-              sizes="100vw"
-              priority
-            />
-          </div>
+    let desktopCategoryBanner;
+    let categoryNameLowerCase = categoryName.toLowerCase();
 
-          <div className="mx-4 lg:mx-0 lg:space-y-10 space-y-4">
-            <SelectedForYou categoryName={categoryName} />
+    // Set Category banner
+    if (categoryNameLowerCase.includes("women")) desktopCategoryBanner = WomenBanner;
+    else if (categoryNameLowerCase.includes("men")) desktopCategoryBanner = MenBanner;
+    else if (categoryNameLowerCase.includes("home")) desktopCategoryBanner = HomeAndGardenBanner;
+    else if (categoryNameLowerCase.includes("electronics")) desktopCategoryBanner = ElectronicsBanner;
+    else if (categoryNameLowerCase.includes("office")) desktopCategoryBanner = OfficeBanner;
+    else if (categoryNameLowerCase.includes("phones")) desktopCategoryBanner = PhonesBanner;
+    else if (categoryNameLowerCase.includes("children")) desktopCategoryBanner = ChildrenBanner;
+    else desktopCategoryBanner = GroceriesBanner;
 
-            <SubCategoriesList categoryName={categoryName} />
-          </div>
-        </div>
-      </div>
-    </InnerPageLayout>
-  );
+    return (
+        <InnerPageLayout>
+            <div className="space-y-4 lg:mb-80 mb-20 mt-20">
+                <div className="lg:mx-8 rounded-lg space-y-6 lg:space-y-10 flex flex-col">
+                    <div className="border h-[200px] lg:h-[280px] relative">
+                        <Image
+                            src={desktopCategoryBanner}
+                            alt="desktop category banner"
+                            className="w-full h-full object-cover object-center hidden lg:block rounded-lg"
+                            fill
+                            sizes="100vw"
+                            priority
+                        />
+                        <Image
+                            src={MobileCategoryBanner}
+                            alt="mobile category banner"
+                            className="w-full h-full object-cover object-center lg:hidden"
+                            fill
+                            sizes="100vw"
+                            priority
+                        />
+                    </div>
+
+                    <div className="mx-4 lg:mx-0 lg:space-y-10 space-y-4">
+                        <SelectedForYou categoryName={categoryName} />
+
+                        <SubCategoriesList categoryName={categoryName} />
+                    </div>
+                </div>
+            </div>
+        </InnerPageLayout>
+    );
 };
 export default Category;
