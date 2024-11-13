65935cd227d0c92e01d97f6654d4cbef003050c1
cprincec <chukwuprincec@gmail.com>
2024-11-13 09:46:41 +0100

    Adjust under 10k deals categories filter to e opened by default

--- components/filters/CategoryFilter.tsx
+++ components/filters/CategoryFilter.tsx
@@ -2,78 +2,83 @@ import { cn } from "@/lib/utils";
 import { Dispatch, SetStateAction, useState } from "react";
 
 interface Category {
-    id: number;
-    name: string;
+  id: number;
+  name: string;
 }
 
 interface CategoryFilterProps {
-    isOpened?: boolean;
-    categories: Category[];
-    category: string;
-    setCategory: Dispatch<SetStateAction<string>>;
+  isOpened?: boolean;
+  categories: Category[];
+  category: string;
+  setCategory: Dispatch<SetStateAction<string>>;
 }
 
-const CategoryFilter: React.FC<CategoryFilterProps> = ({ isOpened, categories, category, setCategory }) => {
-    const [isOpen, setIsOpen] = useState(isOpened || false);
+const CategoryFilter: React.FC<CategoryFilterProps> = ({
+  isOpened,
+  categories,
+  category,
+  setCategory,
+}) => {
+  const [isOpen, setIsOpen] = useState(isOpened || false);
 
-    const toggleDropdown = () => setIsOpen(!isOpen);
+  const toggleDropdown = () => setIsOpen(!isOpen);
 
-    const handleCategorySelect = (category: Category) => {
-        setCategory(category.name.toLocaleUpperCase());
-    };
+  const handleCategorySelect = (category: Category) => {
+    setCategory(category.name.toLocaleUpperCase());
+  };
 
-    return (
-        <div className="relative inline-block text-left w-full transition duration-500 ease-in-out py-6 space-y-4">
-            <div className="flex justify-between items-center w-full">
-                <span className="font-medium">Categories</span>
-                <span
-                    className="bg-kaiglo_grey-100 rounded-full w-6 h-6 flex justify-center items-center cursor-pointer"
-                    onClick={toggleDropdown}
-                >
-                    <svg
-                        className={cn(isOpen && "rotate-180")}
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="12"
-                        height="7"
-                        viewBox="0 0 12 7"
-                        fill="none"
-                    >
-                        <path
-                            d="M5.99706 6.20039C5.5304 6.20039 5.06373 6.02039 4.7104 5.66706L0.36375 1.32042C0.170417 1.12708 0.170417 0.807083 0.36375 0.61375C0.557083 0.420417 0.877083 0.420417 1.07042 0.61375L5.41706 4.96039C5.73706 5.28039 6.25706 5.28039 6.57706 4.96039L10.9237 0.61375C11.1171 0.420417 11.4371 0.420417 11.6304 0.61375C11.8237 0.807083 11.8237 1.12708 11.6304 1.32042L7.28373 5.66706C6.9304 6.02039 6.46373 6.20039 5.99706 6.20039Z"
-                            fill="#151716"
-                        />
-                    </svg>
-                </span>
-            </div>
+  return (
+    <div className="relative inline-block text-left w-full transition duration-500 ease-in-out py-6 space-y-4">
+      <div className="flex justify-between items-center w-full">
+        <span className="font-medium">Categories</span>
+        <span
+          className="bg-kaiglo_grey-100 rounded-full w-6 h-6 flex justify-center items-center cursor-pointer"
+          onClick={toggleDropdown}
+        >
+          <svg
+            className={cn(isOpen && "rotate-180")}
+            xmlns="http://www.w3.org/2000/svg"
+            width="12"
+            height="7"
+            viewBox="0 0 12 7"
+            fill="none"
+          >
+            <path
+              d="M5.99706 6.20039C5.5304 6.20039 5.06373 6.02039 4.7104 5.66706L0.36375 1.32042C0.170417 1.12708 0.170417 0.807083 0.36375 0.61375C0.557083 0.420417 0.877083 0.420417 1.07042 0.61375L5.41706 4.96039C5.73706 5.28039 6.25706 5.28039 6.57706 4.96039L10.9237 0.61375C11.1171 0.420417 11.4371 0.420417 11.6304 0.61375C11.8237 0.807083 11.8237 1.12708 11.6304 1.32042L7.28373 5.66706C6.9304 6.02039 6.46373 6.20039 5.99706 6.20039Z"
+              fill="#151716"
+            />
+          </svg>
+        </span>
+      </div>
 
-            {isOpen && (
-                <div className="w-full text-sm text-kaiglo_grey-base transition duration-500 ease-in-out max-h-80 overflow-hidden overflow-y-auto">
-                    <div
-                        className="py-1 space-y-4"
-                        role="menu"
-                        aria-orientation="vertical"
-                        aria-labelledby="options-menu"
-                    >
-                        {categories.map((item) => (
-                            <p
-                                key={item.id}
-                                className={cn(
-                                    item.name.toLocaleUpperCase() == category && "font-semibold",
-                                    "block py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 cursor-pointer"
-                                )}
-                                role="menuitem"
-                                onClick={() => {
-                                    handleCategorySelect(item);
-                                }}
-                            >
-                                {item.name}
-                            </p>
-                        ))}
-                    </div>
-                </div>
-            )}
+      {isOpen && (
+        <div className="w-full text-sm text-kaiglo_grey-base transition duration-500 ease-in-out max-h-80 overflow-hidden overflow-y-auto">
+          <div
+            className="py-1 space-y-4"
+            role="menu"
+            aria-orientation="vertical"
+            aria-labelledby="options-menu"
+          >
+            {categories.map((item) => (
+              <p
+                key={item.id}
+                className={cn(
+                  item.name.toLocaleUpperCase() == category && "font-semibold",
+                  "block py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 cursor-pointer",
+                )}
+                role="menuitem"
+                onClick={() => {
+                  handleCategorySelect(item);
+                }}
+              >
+                {item.name}
+              </p>
+            ))}
+          </div>
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default CategoryFilter;
--- components/shared/FilterComponentUnder10K.tsx
+++ components/shared/FilterComponentUnder10K.tsx
@@ -5,25 +5,28 @@ import { filterCategories } from "@/constants/categories";
 import CategoryFilter from "../filters/CategoryFilter";
 
 interface Under10KFilterComponentProps {
-    category: string;
-    setCategory: Dispatch<SetStateAction<string>>;
+  category: string;
+  setCategory: Dispatch<SetStateAction<string>>;
 }
 
-const Under10KFilterComponent: React.FC<Under10KFilterComponentProps> = ({ category, setCategory }) => {
-    return (
-        <div className="pt-6 px-4 bg-white rounded-lg w-full divide-y">
-            <h3 className="font-medium bg-kaiglo_info-base text-white text-center px-2 py-3 rounded-xl">
-                Under 10k deals
-            </h3>
+const Under10KFilterComponent: React.FC<Under10KFilterComponentProps> = ({
+  category,
+  setCategory,
+}) => {
+  return (
+    <div className="pt-6 px-4 bg-white rounded-lg w-full divide-y">
+      <h3 className="font-medium bg-kaiglo_info-base text-white text-center px-2 py-3 rounded-xl">
+        Under 10k deals
+      </h3>
 
-            <CategoryFilter
-                isOpened={true}
-                categories={filterCategories}
-                category={category}
-                setCategory={setCategory}
-            />
-        </div>
-    );
+      <CategoryFilter
+        isOpened={true}
+        categories={filterCategories}
+        category={category}
+        setCategory={setCategory}
+      />
+    </div>
+  );
 };
 
 export default Under10KFilterComponent;
