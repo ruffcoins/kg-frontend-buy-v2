622d10f5adc86b03eb381df128d22026f6c900ba
cprincec <chukwuprincec@gmail.com>
2024-11-13 09:31:02 +0100

    changed below-10k to under-10k

--- hooks/queries/products/under10K.tsx
+++ hooks/queries/products/under10K.tsx
@@ -3,54 +3,52 @@ import { IPaginatedProductResponse } from "@/interfaces/responses/product.interf
 import { postRequest } from "@/utils/apiCaller";
 import { useInfiniteQuery } from "@tanstack/react-query";
 
-export const useBelow10KProducts = (filters: ProductFilters) => {
-  const {
-    data,
-    fetchNextPage,
-    hasNextPage,
-    isFetchingNextPage,
-    status,
-    error,
-    refetch,
-    remove,
-    isRefetching,
-    isFetching,
-  } = useInfiniteQuery<IPaginatedProductResponse, Error>(
-    ["below10K-products", filters],
-    ({ pageParam = 0 }) =>
-      postRequest({
-        url: `product/filter/${pageParam}`,
-        payload: filters,
-      }),
-    {
-      getNextPageParam: (lastPage) => {
-        if (lastPage.last) return undefined; // No more pages
-        return lastPage.number + 1; // Next page number
-      },
-      // enabled: !!size,
-      // staleTime: 1000 * 60 * 30,
-      staleTime: 1000 * 60 * 30,
-      cacheTime: 1000 * 60 * 30,
-    },
-  );
+export const useUnder10KProducts = (filters: ProductFilters) => {
+    const {
+        data,
+        fetchNextPage,
+        hasNextPage,
+        isFetchingNextPage,
+        status,
+        error,
+        refetch,
+        remove,
+        isRefetching,
+        isFetching,
+    } = useInfiniteQuery<IPaginatedProductResponse, Error>(
+        ["under10K-products", filters],
+        ({ pageParam = 0 }) =>
+            postRequest({
+                url: `product/filter/${pageParam}`,
+                payload: filters,
+            }),
+        {
+            getNextPageParam: (lastPage) => {
+                if (lastPage.last) return undefined; // No more pages
+                return lastPage.number + 1; // Next page number
+            },
+            // enabled: !!size,
+            // staleTime: 1000 * 60 * 30,
+            staleTime: 1000 * 60 * 30,
+            cacheTime: 1000 * 60 * 30,
+        }
+    );
 
-  const below10KProducts: IProduct[] =
-    data?.pages.flatMap((page) => page.content) ?? [];
-  console.log(below10KProducts);
+    const under10KProducts: IProduct[] = data?.pages.flatMap((page) => page.content) ?? [];
 
-  return {
-    below10KProducts,
-    fetchNextPage,
-    hasNextPage,
-    isFetchingNextPage,
-    isFetching,
-    isRefetching,
-    status,
-    error,
-    refetchbelow10KProducts: refetch,
-    removebelow10KProducts: remove,
-    totalProducts: data?.pages[data.pages.length - 1]?.totalElements,
-    currentPage: data?.pages[data.pages.length - 1]?.number as number,
-    totalPages: data?.pages[data.pages.length - 1]?.totalPages,
-  };
+    return {
+        under10KProducts,
+        fetchNextPage,
+        hasNextPage,
+        isFetchingNextPage,
+        isFetching,
+        isRefetching,
+        status,
+        error,
+        refetchUnder10KProducts: refetch,
+        removeUnderKProducts: remove,
+        totalProducts: data?.pages[data.pages.length - 1]?.totalElements,
+        currentPage: data?.pages[data.pages.length - 1]?.number as number,
+        totalPages: data?.pages[data.pages.length - 1]?.totalPages,
+    };
 };
